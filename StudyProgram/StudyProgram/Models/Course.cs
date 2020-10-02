aa449f644c14ff7118f793693c9e35a04e9552ae
ThongDT <longdt555@gmail.com>
2020-10-02 22:55:43 +0700

    m

---
+++ StudyProgram/StudyProgram/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyProgram.Models
+{
+    public class Course
+    {  
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get;set }
+
+
+    }
+}
