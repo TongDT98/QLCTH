ad3478d75495ed1dbda740538487c8fa1a03f0e8
TongDT <dinhthethong050698@gmail.com>
2020-10-14 23:54:24 +0700

    update spec

---
+++ StudyProgram/StudyProgram/Controllers/SpecialityController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudyProgram.DataContext;
+using StudyProgram.Entity;
+
+namespace StudyProgram.Controllers
+{
+    public class SpecialityController : Controller
+    {
+        private readonly SPMContext _context;
+
+        public SpecialityController(SPMContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Speciality
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Speciality.ToListAsync());
+        }
+
+        // GET: Speciality/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var speciality = await _context.Speciality
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (speciality == null)
+            {
+                return NotFound();
+            }
+
+            return View(speciality);
+        }
+
+        // GET: Speciality/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Speciality/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SpecialityId,NameVN,NameEN,Desciption,Id,Isdeleted")] Speciality speciality)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(speciality);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(speciality);
+        }
+
+        // GET: Speciality/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var speciality = await _context.Speciality.FindAsync(id);
+            if (speciality == null)
+            {
+                return NotFound();
+            }
+            return View(speciality);
+        }
+
+        // POST: Speciality/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SpecialityId,NameVN,NameEN,Desciption,Id,Isdeleted")] Speciality speciality)
+        {
+            if (id != speciality.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(speciality);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SpecialityExists(speciality.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(speciality);
+        }
+
+        // GET: Speciality/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var speciality = await _context.Speciality
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (speciality == null)
+            {
+                return NotFound();
+            }
+
+            return View(speciality);
+        }
+
+        // POST: Speciality/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var speciality = await _context.Speciality.FindAsync(id);
+            _context.Speciality.Remove(speciality);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SpecialityExists(int id)
+        {
+            return _context.Speciality.Any(e => e.Id == id);
+        }
+    }
+}
